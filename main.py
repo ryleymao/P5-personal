5ca7d787b563134c57477f2b4627b9a9c97c8983
Ryley Mao <rymao_@outlook.com>
2025-09-17 03:21:42 +0000

    Define important classes

---
+++ main.py
@@ -0,0 +1,8 @@
+class Exercise:
+    def __init__(self, muscle: str, sets: int, reps: int):
+        self.muscleGroup = muscleGroup
+        self.sets = sets
+        self.reps = reps
+
+class Workout:
+    def __init__(self)
